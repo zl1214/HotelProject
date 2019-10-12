6635c26569adacf433d366b5bed02ce00b44be8b
zj <956231558@qq.com>
2019-10-12 17:29:59 +0800

    1.权限管理布局及侧边栏伸缩

---
+++ Layui_AuthorityManage/Layui_AuthorityManage/Content/lib/model/home.js
@@ -0,0 +1,37 @@
+﻿layui.define(['element','jquery'], function (exports) {
+    var element = layui.element,$=layui.$;
+
+    var $body = $('body');
+
+    var events = {
+        //侧边导航栏伸缩事件
+        flexible: function (that) {
+            var sideShinkClass = 'layadmin-side-shrink',
+                shrinkClassIcon = 'layui-icon-shrink-right',
+                spreadClassIcon = 'layui-icon-spread-left';
+
+            var $container = $('#cntr_Container'),
+                $iconFlexible = $('#icon_flexible'),
+                isFlexible = $container.hasClass(sideShinkClass);
+            if (isFlexible) {
+                $container.removeClass(sideShinkClass);//展开导航栏
+                $iconFlexible.removeClass(spreadClassIcon).addClass(shrinkClassIcon);
+            }
+            else
+            {
+                $container.addClass(sideShinkClass);//收缩导航栏
+                $iconFlexible.addClass(spreadClassIcon).removeClass(shrinkClassIcon);
+            }
+        }
+    };
+    //注册全局事件
+    $body.on('click', '*[click-event]', function () {
+        var that = $(this);
+        //获取自定义的事件名称
+        var eventAttr = that.attr('click-event');
+        //执行事件
+        events[eventAttr] && events[eventAttr].call(this,that);
+    });
+
+    exports('home',null)
+})
